d0501586267b8b4cee2fbad8c36f2cb7908892ea
Ben Thompson <ben@invertedtomato.com>
2018-01-02 07:42:01 +1000

    Added unit tests.

---
+++ LibraryTests/BinaryMessageTests.cs
@@ -0,0 +1,27 @@
+﻿using InvertedTomato.IO.Messages;
+using System;
+using Xunit;
+
+namespace LibraryTests {
+    public class BinaryMessageTests {
+        [Fact]
+        public void Initialize() {
+            var msg = new BinaryMessage(new Byte[] { 1, 2, 3, });
+            Assert.Equal(new Byte[] { 1, 2, 3 }, msg.Value);
+        }
+
+        [Fact]
+        public void Import() {
+            var msg = new BinaryMessage();
+            msg.Import(new Byte[] { 1, 2, 3, });
+            Assert.Equal(new Byte[] { 1, 2, 3 }, msg.Value);
+        }
+
+        [Fact]
+        public void Export() {
+            var msg = new BinaryMessage();
+            msg.Value = new Byte[] { 1, 2, 3, };
+            Assert.Equal(new Byte[] { 1, 2, 3 }, msg.Export());
+        }
+    }
+}
---
+++ LibraryTests/StringMessageTests.cs
@@ -0,0 +1,27 @@
+﻿using InvertedTomato.IO.Messages;
+using System;
+using Xunit;
+
+namespace LibraryTests {
+    public class StringMessageTests {
+        [Fact]
+        public void Initialize() {
+            var msg = new StringMessage("test");
+            Assert.Equal("test", msg.Value);
+        }
+
+        [Fact]
+        public void Import() {
+            var msg = new StringMessage();
+            msg.Import(new Byte[] { 116, 101, 115, 116 });
+            Assert.Equal("test", msg.Value);
+        }
+
+        [Fact]
+        public void Export() {
+            var msg = new StringMessage();
+            msg.Value = "test";
+            Assert.Equal(new Byte[] { 116, 101, 115, 116 }, msg.Export());
+        }
+    }
+}
