8ad8a9ccc1e187d02d91be1bae2df7b64c74aea3
Ben Thompson <ben@invertedtomato.com>
2018-01-03 07:54:46 +1000

    Added ProtoBufMessage, for easy serialization/deserialization of objects.

---
+++ Library/IO/Messages/ProtoBufMessage.cs
@@ -0,0 +1,51 @@
+﻿using ProtoBuf;
+using System;
+using System.IO;
+
+namespace InvertedTomato.IO.Messages {
+    public class ProtoBufMessage<T> : IMessage {
+        public Byte[] Payload { get; set; }
+
+        public ProtoBufMessage() { }
+        public ProtoBufMessage(T value) {
+            Serialize(value);
+        }
+
+        public void Serialize(T value) {
+            if(null == value) {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            using(var stream = new MemoryStream()) {
+                Serializer.Serialize(stream, value);
+                Payload = stream.ToArray();
+            }
+        }
+
+        public T Deserialize() {
+            if(null == Payload) {
+                throw new InvalidOperationException("No value set.");
+            }
+
+            using(var stream = new MemoryStream(Payload)) {
+                return Serializer.Deserialize<T>(stream);
+            }
+        }
+
+        public Byte[] Export() {
+            if(null == Payload) {
+                throw new InvalidOperationException("No value set.");
+            }
+
+            return Payload;
+        }
+
+        public void Import(Byte[] payload) {
+            if(null == payload) {
+                throw new ArgumentNullException(nameof(payload));
+            }
+
+            Payload = payload;
+        }
+    }
+}
---
+++ LibraryTests/ProtoBufMessageTests.cs
@@ -0,0 +1,44 @@
+﻿using InvertedTomato.IO.Messages;
+using ProtoBuf;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LibraryTests {
+    public class ProtoBufMessageTests {
+        [Fact]
+        public void Initialize() {
+            var msg = new ProtoBufMessage<Message>(new Message() {
+                Value = 1
+            });
+
+            Assert.Equal(new Byte[] { 8, 1 }, msg.Export());
+        }
+
+        [Fact]
+        public void Import() {
+            var msg = new ProtoBufMessage<Message>();
+            msg.Import(new Byte[] { 8, 1 });
+            Assert.Equal((UInt32)1, msg.Deserialize().Value);
+        }
+
+        [Fact]
+        public void Export() {
+            var msg = new ProtoBufMessage<Message>();
+            msg.Serialize(new Message() {
+                Value = 1
+            });
+            Assert.Equal(new Byte[] { 8, 1 }, msg.Export());
+        }
+    }
+
+    [ProtoContract]
+    public class Message {
+        public Message() { }
+        public Message(UInt32 value) { Value = value; }
+
+        [ProtoMember(1)]
+        public UInt32 Value { get; set; }
+    }
+}
